3fd1b64d99774df3b9945d6d9bf2e5d01773c238
Andreas <Andreas.Heuermann@fh-bielefeld.de>
2019-01-10 18:58:20 +0100

    Fixed wrong types in ccall in Wrapper function

--- src/FMIWrapper.jl
+++ src/FMIWrapper.jl
@@ -8,20 +8,20 @@ include("FMICallbackFunctions.jl") # Callbacks for logging and memory handling
 
 function fmi2Instantiate(libHandle::Ptr{Nothing}, instanceName::String,
     fmuType::fmuType, fmuGUID::String, fmuResourceLocation::String,
-    functions::CallbackFunctions, visible::Bool, loggingOn::Bool)
+    functions::CallbackFunctions, visible::Bool=true, loggingOn::Bool=false)
 
     func = dlsym(libHandle, :fmi2Instantiate)
 
-    fmi2Componente = ccall(
+    fmi2Component = ccall(
       func,
-      Ref{Cvoid},
+      Ptr{Cvoid},
       (Cstring, Cint, Cstring, Cstring,
       Ref{CallbackFunctions}, Cint, Cint),
       instanceName, fmuType, fmuGUID, fmuResourceLocation,
       Ref(functions), visible, loggingOn
       )
 
-    if fmi2Componente == C_NULL
+    if fmi2Component == C_NULL
       error("fmi2Instantiate: Returned NULL.")
     end
 end
