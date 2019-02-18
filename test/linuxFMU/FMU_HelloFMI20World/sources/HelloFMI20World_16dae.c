5ef71ff53a52cd59f916dc49cfb1111d1bba4bc6
AnHeuermann <andreas.heuermann@fh-bielefeld.de>
2019-02-18 16:21:09 +0100

    Removed temp files generated by FMUs

--- test/linuxFMU/FMU_HelloFMI20World/sources/HelloFMI20World_16dae.c
+++
@@ -1,9 +0,0 @@
-/* DAE residuals is empty */
- #include "HelloFMI20World_model.h"
-#ifdef __cplusplus
-extern "C" {
-#endif
-int HelloFMI20World_initializeDAEmodeData(DATA *inData, DAEMODE_DATA* daeModeData){ return -1; }
-#ifdef __cplusplus
-}
-#endif
