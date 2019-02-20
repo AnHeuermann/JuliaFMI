095534e290364ead536b5072bdd074614e864289
AnHeuermann <andreas.heuermann@fh-bielefeld.de>
2019-02-20 12:12:30 +0100

    Moved single tests to new file and added unit testing
      - Test are moved to new file test/tests.jl
      - Using Base.Test for unit testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,27 +1,16 @@
 """
 Run all tests for simulating FMU's
-    ToDo: Add a working simulator :-(
 """
 
-thisDir = dirname(Base.source_path())
-include("$(dirname(thisDir))/src/FMUSimulator.jl")
+using Test
 
+include("tests.jl")
 
-if Sys.iswindows()
-    fmuTestDir = string(thisDir, "/winFMU")
-elseif Sys.islinux()
-    fmuTestDir = string(thisDir, "/linuxFMU")
-else
-    error("OS not supportet for this tests.")
-end
-
-# First simple test
-function testHelloFMI20World()
-    helloFMI20World = string(fmuTestDir,"/HelloFMI20World.fmu")
-    main(helloFMI20World)
-end
-
-function testBouncingBall()
-    bouncingBall = string(fmuTestDir,"/BouncingBallFMI20.fmu")
-    main(bouncingBall)
-end
+@testset "Simulating simple FMUs" begin
+    @testset "Without events" begin
+        @test testHelloFMI20World()
+    end;
+    @testset "With events" begin
+        @test testBouncingBall()
+    end;
+end;
