9b1163f1f10ad763ae09dffe81e068964e2da3f6
AnHeuermann <andreas.heuermann@fh-bielefeld.de>
2019-02-20 12:12:30 +0100

    Moved single tests to new file and added unit testing
      - Test are moved to new file test/tests.jl
      - Using Base.Test for unit testing

---
+++ test/tests.jl
@@ -0,0 +1,67 @@
+"""
+Define tests for unit testing
+"""
+
+thisDir = dirname(Base.source_path())
+include("$(dirname(thisDir))/src/FMUSimulator.jl")
+
+
+if Sys.iswindows()
+    fmuTestDir = string(thisDir, "/winFMU")
+elseif Sys.islinux()
+    fmuTestDir = string(thisDir, "/linuxFMU")
+else
+    error("OS not supportet for this tests.")
+end
+
+"""
+    testHelloFMI20World()
+
+Run most basic simulation test.
+
+FMU generated for Modelica model:
+```
+    model HelloFMI20World
+      Real x(start=1);
+      parameter Real a=2;
+    equation
+      der(x) = a * x;
+    end HelloFMI20World;
+```
+"""
+function testHelloFMI20World()
+    helloFMI20World = string(fmuTestDir,"/HelloFMI20World.fmu")
+    main(helloFMI20World)
+    return true
+end
+
+
+"""
+    testBouncingBall()
+
+Run simple test simulating a bouncing ball.
+
+FMU generated from Modelica model:
+```
+    model BouncingBallFMI20
+      parameter Real e=0.7 \"coefficient of restitution\";
+      parameter Real g=9.81 \"gravity acceleration\";
+      Real h(start=1) \"height of ball\";
+      Real v \"velocity of ball\";
+      Real v_new;
+    equation
+      der(v) = -g;
+      der(h) = v;
+
+      when h <= 0.0 then
+        v_new = -e*pre(v);
+        reinit(v, v_new);
+      end when;
+    end BouncingBallFMI20;
+```
+"""
+function testBouncingBall()
+    bouncingBall = string(fmuTestDir,"/BouncingBallFMI20.fmu")
+    main(bouncingBall)
+    return true
+end
