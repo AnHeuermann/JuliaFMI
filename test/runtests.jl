485932b8ce7fec7eed949b575277a909760cc97f
Andreas Heuermann <andreas.heuermann@fh-bielefeld.de>
2019-03-20 19:10:43 +0100

    Use FMUs provided by modelica/fmi-cross-check for testing
      - Tests for Linux and Windows, 32 and 64 bit systems
      - Tests are broken at the moment

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,12 +5,17 @@ Run all tests for simulating FMU's
 using Test
 
 include("tests.jl")
+include("fmi-cross-check-tests.jl")
 
-@testset "Simulating simple FMUs" begin
-    @testset "Without events" begin
-        @test testHelloFMI20World()
+@testset "All Tests" begin
+    @testset "Simulating simple FMUs" begin
+        @testset "Without events" begin
+            @test testHelloFMI20World()
+        end;
+        @testset "With events" begin
+            @test testBouncingBall()
+        end;
     end;
-    @testset "With events" begin
-        @test testBouncingBall()
-    end;
+
+    runFMICrossTests()
 end;
