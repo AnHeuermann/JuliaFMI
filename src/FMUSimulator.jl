5ba3d359c1be4bd2b373f3dad4293870a7754ea1
AnHeuermann <andreas.heuermann@fh-bielefeld.de>
2019-11-19 16:38:31 +0100

    Added enumerationVariables
      - Added parsing of TypeDefinitions for Enumerations
      - Moved modelDescription parsing to own file
        - Split parts of readModelDescription into own functions.
      - Initialize enumerationData in simulationData

--- src/FMUSimulator.jl
+++ src/FMUSimulator.jl
@@ -9,6 +9,7 @@ using LightXML      # For parsing XML files
 export main
 
 include("FMIWrapper.jl")
+include("modelDescriptionParsing.jl")
 
 # Macro to identify logger library
 macro libLogger()
@@ -21,288 +22,6 @@ macro libLogger()
     else
         error("OS not supported")
     end
-end
-
-"""
-Parse modelDescription.xml
-"""
-function readModelDescription(pathToModelDescription::String)
-
-    md = ModelDescription()
-
-    if !isfile(pathToModelDescription)
-        error("File $pathToModelDescription does not exist.")
-    elseif basename(pathToModelDescription) != "modelDescription.xml"
-        error("File name is not equal to \"modelDescription.xml\" but $(basename(pathToModelDescription))" )
-    end
-
-    # Parse modelDescription
-    xdoc = parse_file(pathToModelDescription)
-
-    # Check root tag
-    xroot = root(xdoc)
-    if name(xroot)!="fmiModelDescription"
-        error("modelDescription.xml root is not \"fmiModelDescription\".")
-    end
-
-    # Get attributes of tag <fmiModelDescription>
-    attributes = attributes_dict(xroot)
-    try
-        md.fmiVersion = attributes["fmiVersion"]
-        md.modelName = attributes["modelName"]
-        md.guid = attributes["guid"]
-
-        md.description = get(attributes, "description", "")
-        md.author = get(attributes, "author", "")
-        md.version = get(attributes, "version", "")
-        md.copyright = get(attributes, "copyright", "")
-        md.license = get(attributes, "license", "")
-        md.generationTool = get(attributes, "generationTool", "")
-        md.generationDateAndTime = get(attributes, "generationDateAndTime", "")
-        md.variableNamingConvention = NamingConvention(get(attributes, "variableNamingConvention", "flat"))
-        md.numberOfEventIndicators = parse(Int, attributes["numberOfEventIndicators"])
-
-        # Get attributes of tag <ModelExchange>
-        elementModelExchange = find_element(xroot, "ModelExchange")
-
-        # Get attributes of tag <CoSimulation>
-        elementCoSimulation = find_element(xroot, "CoSimulation")
-
-        try
-            if elementModelExchange != nothing
-                md.isModelExchange = true
-                md.modelIdentifier = attribute(elementModelExchange, "modelIdentifier"; required=true)
-            else
-                md.isModelExchange = false
-                error("While parsing modelDescription: Only ModelExchange is supported yet but
-                    FMU does not support ModelExchange.")
-            end
-
-            if elementCoSimulation != nothing
-                md.isCoSimulation = true
-                md.modelIdentifier = attribute(elementModelExchange, "modelIdentifier"; required=true)
-            else
-                md.isCoSimulation = false
-            end
-        catch err
-            if isa(err, LightXML.XMLAttributeNotFound)
-                error("While parsing modelDescription: Non-optinal element \"modelIdentifier\" not found")
-            else
-                rethrow(err)
-            end
-        end
-
-        if elementCoSimulation === nothing && elementModelExchange === nothing
-            error("modelDescription.xml is missing ModelExchange and CoSimulation tags.")
-        end
-
-        # ToDo: Add stuff for tag UnitDefinitions
-
-        # ToDo: Add stuff for tag TypeDefinitions
-
-        # Get attributes of tag LogCategories
-        elementLogCategories = find_element(xroot, "LogCategories")
-        if elementLogCategories != nothing
-            numCategories = 0
-            for element in child_elements(elementLogCategories)
-                numCategories += 1
-            end
-            md.logCategories = Array{LogCategory}(undef, numCategories)
-
-            for (index, element) in enumerate(child_elements(elementLogCategories))
-                tmp_name = attribute(element, "name"; required=true)
-                tmp_description = attribute(element, "description"; required=false)
-                if tmp_description === nothing
-                    md.logCategories[index] = LogCategory(tmp_name)
-                else
-                    md.logCategories[index] = LogCategory(tmp_name, tmp_description)
-                end
-            end
-        end
-
-        # Get attributes of tag DefaultExperiment
-        elementDefaultExperiment = find_element(xroot, "DefaultExperiment")
-        if elementDefaultExperiment != nothing
-            startTime = attribute(elementDefaultExperiment, "startTime"; required=false)
-            if startTime != nothing
-                startTime = parse(Float64, startTime)
-            else
-                startTime = 0
-            end
-
-            stopTime = attribute(elementDefaultExperiment, "stopTime"; required=false)
-            if startTime != nothing
-                stopTime = parse(Float64, stopTime)
-            else
-                stopTime = startTime+1
-            end
-
-            tolerance = attribute(elementDefaultExperiment, "tolerance"; required=false)
-            if tolerance != nothing
-                tolerance = parse(Float64, tolerance)
-            else
-                tolerance = 1e-6
-            end
-
-            stepSize = attribute(elementDefaultExperiment, "stepSize"; required=false)
-            if stepSize != nothing
-                stepSize = parse(Float64, stepSize)
-            else
-                stepSize = 2e-3
-            end
-
-            md.defaultExperiment = ExperimentData(startTime, stopTime, tolerance, stepSize)
-        else
-            md.defaultExperiment = ExperimentData()
-        end
-
-        # ToDo: Add stuff for tag VendorAnnotations
-
-        # Get attributes of tag ModelVariables
-        elementModelVariables = find_element(xroot, "ModelVariables")
-        if elementModelVariables === nothing
-            error("modelDescription.xml is missing ModelVariables tag")
-        end
-
-        numberOfVariables = 0
-        for element in get_elements_by_tagname(elementModelVariables, "ScalarVariable")
-            numberOfVariables += 1
-        end
-        scalarVariables = Array{ScalarVariable}(undef, numberOfVariables)
-
-        for (index, element) in enumerate(get_elements_by_tagname(elementModelVariables, "ScalarVariable"))
-            # Get attributes name, valueReference, variability, causality, initial
-            tmp_name = attribute(element, "name"; required=true)
-            tmp_valueReference = parse(Int, attribute(element, "valueReference"; required=true))
-
-            tmp_description = attribute(element, "description"; required=false)
-            if tmp_description === nothing
-                tmp_description=""
-            end
-            tmp_variability = attribute(element, "variability"; required=false)
-            if tmp_variability === nothing
-                tmp_variability=""
-            end
-            tmp_causality = attribute(element, "causality"; required=false)
-            if tmp_causality === nothing
-                tmp_causality=""
-            end
-            tmp_initial = attribute(element, "initial"; required=false)
-            if tmp_initial === nothing
-                tmp_initial=""
-            end
-            tmp_canHandleMultipleSetPerTimelnstant = attribute(element, "canHandleMultipleSetPerTimelnstant"; required=false)
-            if tmp_canHandleMultipleSetPerTimelnstant === nothing
-                tmp_canHandleMultipleSetPerTimelnstant = false
-            else
-                tmp_canHandleMultipleSetPerTimelnstant = parse(Bool,
-                    tmp_canHandleMultipleSetPerTimelnstant)
-            end
-
-            # Get child node for typeSpecificProperties
-            tmp_typeSpecificProperties = nothing
-            for child in child_elements(element)
-                if is_elementnode(child)
-                    if name(child)=="Real"
-                        tmp_declaredType = "Real"
-                        tmp_variableAttributes = RealAttributes()   # TODO implement
-                        tmp_start = attribute(child, "start"; required=false)
-                        if tmp_start === nothing
-                            tmp_start = Float64(0)
-                        else
-                            tmp_start = parse(Float64, tmp_start)
-                        end
-                        tmp_derivative = attribute(child, "derivative"; required=false)
-                        if tmp_derivative === nothing
-                            tmp_derivative = UInt(0)
-                        else
-                            tmp_derivative = parse(UInt, tmp_derivative)
-                        end
-                        tmp_reinit = attribute(child, "reinit"; required=false)
-                        if tmp_reinit === nothing
-                            tmp_reinit = false
-                        end
-                        tmp_typeSpecificProperties = RealProperties(tmp_declaredType, tmp_variableAttributes, tmp_start, tmp_derivative, tmp_reinit)
-                    elseif name(child)=="Integer"
-                        tmp_declaredType = "Integer"
-                        tmp_variableAttributes = IntegerAttributes()   # TODO implement
-                        tmp_start = attribute(child, "start"; required=false)
-                        if tmp_start === nothing
-                            tmp_start = Int(0)
-                        else
-                            tmp_start = parse(Int, tmp_start)
-                        end
-                        tmp_typeSpecificProperties = IntegerProperties(tmp_declaredType, tmp_variableAttributes, tmp_start)
-                    elseif name(child)=="Boolean"
-                        tmp_declaredType = "Boolean"
-                        tmp_start = attribute(child, "start"; required=false)
-                        if tmp_start === nothing
-                            tmp_start = false
-                        else
-                            tmp_start = parse(Bool, tmp_start)
-                        end
-                        tmp_typeSpecificProperties = BooleanProperties(tmp_declaredType, tmp_start)
-                    elseif name(child)=="String"
-                        tmp_declaredType = "String"
-                        tmp_start = attribute(child, "start"; required=false)
-                        if tmp_start === nothing
-                            tmp_start = ""
-                        end
-                        tmp_typeSpecificProperties = StringProperties(tmp_declaredType, tmp_start)
-                    elseif name(child)=="Enumeration"
-                        tmp_declaredType = "Enumeration"
-                        tmp_quantity = attribute(child, "quantity"; required=false)
-                        if tmp_quantity === nothing
-                            tmp_quantity = ""
-                        end
-                        tmp_min = attribute(child, "min"; required=false)
-                        if tmp_min === nothing
-                            tmp_min = Int(0)
-                        else
-                            tmp_min = parse(Int, tmp_min)
-                        end
-                        tmp_max = attribute(child, "max"; required=false)
-                        if tmp_max === nothing
-                            tmp_max = Int(0)
-                        else
-                            tmp_max = parse(Int, tmp_max)
-                        end
-                        tmp_start = attribute(child, "start"; required=false)
-                        if tmp_start === nothing
-                            tmp_start = Int(0)
-                        else
-                            tmp_start = parse(Int, tmp_start)
-                        end
-                        tmp_typeSpecificProperties = EnumerationProperties(tmp_declaredType, tmp_quantity, tmp_min, tmp_max, tmp_start)
-                    else
-                        error("Unknown type \"$(name(child))\" of ScalarVariable")
-                    end
-                end
-            end
-
-            scalarVariables[index] = ScalarVariable(tmp_name,
-                tmp_valueReference, tmp_description, tmp_causality,
-                tmp_variability, tmp_initial,
-                tmp_canHandleMultipleSetPerTimelnstant,
-                tmp_typeSpecificProperties)
-        end
-        md.modelVariables = scalarVariables
-
-        # Get attributes of tag ModelStructure
-
-
-    catch err
-        if isa(err, KeyError)
-            error("While parsing modelDescription: Non-optional element \"$(err.key)\" not found")
-        else
-            rethrow(err)
-        end
-    end
-
-    # Free memory
-    LightXML.free(xdoc)
-
-    return md
 end
 
 
@@ -353,7 +72,8 @@ function initializeSimulationData(modelDescription::ModelDescription,
 
     simulationData = SimulationData(modelData.numberOfReals,
         modelData.numberOfInts, modelData.numberOfBools,
-        modelData.numberOfStrings, 0, modelData.numberOfEventIndicators)    # TODO Add number of enumerations here!
+        modelData.numberOfStrings, modelData.numberOfEnumerations,
+        modelData.numberOfEventIndicators)
 
     i_real = i_int = i_bool = i_string = i_enumertion = 0
 
@@ -385,11 +105,11 @@ function initializeSimulationData(modelDescription::ModelDescription,
                                scalarVar.valueReference,
                                scalarVar.name)
         elseif typeof(scalarVar.typeSpecificProperties) == EnumerationProperties
-            error("Enumeration variables not implemeted!")
-            #i_enumertion += 1
-            # TODO add enumerations here
-            #simulationData.modelVariables.enumerations[i_enumertion] =
-            #    EnumerationVariable()
+            i_enumertion += 1
+            simulationData.modelVariables.enumerations[i_enumertion] =
+                EnumerationVariable(scalarVar.typeSpecificProperties.start,
+                                    scalarVar.valueReference,
+                                    scalarVar.name)
         else
             error("Unknown scalar variable type $(typeof(scalarVar.typeSpecificProperties)).")
         end
@@ -403,8 +123,8 @@ function initializeSimulationData(modelDescription::ModelDescription,
         error("Counted number of boolean scalar variables $i_bool didn't matched expeted $(modelData.numberOfBools)")
     elseif i_string != modelData.numberOfStrings
         error("Counted number of string scalar variables $i_string didn't matched expeted $(modelData.numberOfStrings)")
-    elseif i_enumertion != 0
-        error("Counted number of enumeration scalar variables $i_enumertion didn't matched expeted 0")
+    elseif i_enumertion != modelData.numberOfEnumerations
+        error("Counted number of enumeration scalar variables $i_enumertion didn't matched expeted $(modelData.numberOfEnumerations)")
     end
 
     return simulationData
