0331950aebff160b1becc3fe692f496749bdfc9f
Cook, James <james.cook@ukaea.uk>
2021-02-03 18:53:59 +0000

    remove GC calls in runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,24 +12,19 @@ using Test
 include("tests.jl")
 include("fmi-cross-check-tests.jl")
 
-GC.enable(false)
 
 @testset "All Tests" begin
     @testset "Simulating simple FMUs" begin
         @testset "Without events" begin
             testHelloFMI20World()
-            GC.safepoint()
             if Sys.islinux()
                 testCauerLowPassAnalog()
-                GC.safepoint()
             end
         end
         @testset "With events" begin
             testBouncingBall()
-            GC.safepoint()
         end
     end
 
-    GC.safepoint()
     runFMICrossTests()
 end
